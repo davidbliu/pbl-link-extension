be0eb6c94c1965930c569afb2e76ffeee2178850
David Liu <davidbliu@gmail.com>
2015-08-03 20:46:41 -0700

    add omnibox

--- background.js
+++ background.js
@@ -1,14 +1,31 @@
 
 
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-    // chrome.tabs.executeScript(tabId, {file: "bundles.js"} );
-    console.log('this is a background page');
-    $('.link-row').each(function(){
-      console.log('this is a link row');
+// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+//     // chrome.tabs.executeScript(tabId, {file: "bundles.js"} );
+//     console.log('this is a background page');
+//     $('.link-row').each(function(){
+//       console.log('this is a link row');
+//     });
+//     console.log('there are this many link rowrs');
+//     $('.link-row').click(function(){
+//       alert('hi');
+//     });
+// });
+
+  function resetDefaultSuggestion() {
+    chrome.omnibox.setDefaultSuggestion({
+    description: 'go: Search PBL Links for %s'
     });
-    console.log('there are this many link rowrs');
-    $('.link-row').click(function(){
-      alert('hi');
+  }
+  resetDefaultSuggestion();
+
+  function navigate(url) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
     });
-});
+  }
+
+  chrome.omnibox.onInputEntered.addListener(function(text) {
+    navigate("http://pbl.link/" + text);
+  });
