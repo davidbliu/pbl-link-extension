fba299f92513adb3e2e0a4eb19c7ea8fc22e80e0
David Liu <davidbliu@gmail.com>
2015-09-03 00:16:47 -0700

    version 1.0

--- background.js
+++ background.js
@@ -1,9 +1,89 @@
 
-var email = 'none'
+
+
+var root_url = 'http://testing.berkeley-pbl.com/';
+var notification_hash = {};
+// var root_url = 'http://localhost:3000/'
+
+var email = 'none';
 chrome.identity.getProfileUserInfo(function(userInfo) {
- /* Use userInfo.email, or better (for privacy) userInfo.id
-    They will be empty if user is not signed in in Chrome */
+  // Use userInfo.email, or better (for privacy) userInfo.id
+    // They will be empty if user is not signed in in Chrome 
     email = userInfo.email;
+});
+
+function receiver(message) {
+  var opt = {
+    type: "basic",
+    title: "PBL Links Notifier",
+    message: message,
+    iconUrl: "pbl-logo-circle.png"
+  }
+  chrome.notifications.create('my_notification', opt);
+}
+
+function createPushNotification(notification){
+  // console.log("creating push notification");
+  if(notification.type != null && notification.type == 'links'){
+    // console.log('printing notification details');
+    // console.log(notification.message);
+    // console.log(notification.timestamp);
+    // console.log(notification.key);
+    items = [{ title: "Message", message: notification.message},
+              { title: "Sent From", message: notification.sender},
+              { title: "Timestamp", message: notification.timestamp},
+              { title: "Links", message: notification.key}];
+
+
+    links = notification.key.split(',');
+    for(var i=0;i<links.length;i++){
+      links[i] = root_url + links[i].trim() + '/go'
+    }
+    notification_hash[notification.id] = links;
+
+    console.log('here');
+    var opt = {
+      type: "list",
+      title: notification.title,
+      message: notification.message,
+      iconUrl: "pbl-logo-circle.png",
+      items: items
+    }
+
+    // var notif = new Notification(
+    //   notification.title, {
+    //   type:'list',
+    //   icon: 'pbl-logo-circle.png',
+    //   body:notification.message,
+    // });
+    
+    var notif = chrome.notifications.create(notification.id, opt);
+    // console.log(notification.id);
+    // console.log('that was the id');
+    
+    // console.log(links);
+    // console.log('thos were the links received');
+    // // var link = root_url + notification.key + '/go'
+    // chrome.notifications.onButtonClicked.addListener(function(){
+    //   for(var i=0;i<links.length;i++){
+    //     window.open(links[i]);
+    //   }
+    // });
+    // notif.onclick = function () {
+    //   for(var i=0;i<links.length;i++){
+    //     window.open(links[i]);
+    //   }
+    // };
+   }   
+}
+
+chrome.notifications.onClicked.addListener(function (data){
+  links = notification_hash[data];
+  for(var i=0;i<links.length;i++){
+        window.open(links[i]);
+      }
+  delete notification_hash[data];
+
 });
 
 var url = '';
@@ -24,9 +104,6 @@ function createCORSRequest(method, url) {
   return xhr;
 }
 
-// var root_url = "http://localhost:3000/"
-var root_url = 'http://testing.berkeley-pbl.com/'
-
   function resetDefaultSuggestion() {
     chrome.omnibox.setDefaultSuggestion({
     description: 'go: Search PBL Links for %s'
@@ -45,32 +122,83 @@ var root_url = 'http://testing.berkeley-pbl.com/'
       key = text.split('add:')[1].trim();
       chrome.tabs.getSelected(null,function(tab) {
             url = tab.url;
-            var description = prompt("Saving your key: " + key + " with the URL: " + url + ". Would you like to add a description?", "no description");
-            if (description!=null)
-            {
-              params = "key="+encodeURIComponent(key)+"&url="+encodeURIComponent(url)+'&email='+encodeURIComponent(email)+ "&description="+description
-              post_url = root_url + 'chrome/create_go_link' + '?' + params  
-              var xhr = createCORSRequest('POST', post_url); 
-              if (!xhr) {
-                console.log('unable to send CORS request');
-                return;
-              }
-              xhr.onload = function() {
-                alert('successfully saved link');
-              };
-              xhr.onerror = function() {
-                alert('unable to save link');
-              };
-              xhr.send();
-
+            if(key == ""){
+              key = tab.title;
             }
+            navigate(root_url + "/go/add_landing_page?key="+encodeURIComponent(key)+"&url="+encodeURIComponent(url));
             
       });
 
     }
     else{
-      navigate("http://pbl.link/" + text);
+      // navigate("http://pbl.link/" + text);
+      navigate(root_url+text+'/go');
     }
   });
 
+
+//register for push notificaitons
+function registerCallback(registrationId) {
+  if (chrome.runtime.lastError) {
+    // When the registration fails, handle the error and retry the
+    // registration later.
+    console.log('registration failed, will try again later');
+    return;
+  }
+  console.log('the registration id was');
+  console.log(registrationId);
+  chrome.storage.local.set({registered: true});
+  chrome.storage.local.set({'registrationId': registrationId});
+  sendRegistrationId(registrationId);
+
+}
+
+function sendRegistrationId(registrationId) {
+  // Send the registration token to your application server
+  // in a secure way.
+  //send registration token and email
+  console.log('email is '+email);
+  params = 'email='+email+'&registration_id='+registrationId;
+  post_url = root_url + 'chrome/register_notification_client' + '?' + params  
+  var xhr = createCORSRequest('POST', post_url); 
+  if (!xhr) {
+    console.log('unable to send CORS request');
+    return;
+  }
+  xhr.onload = function() {
+    alert('successfully registered for notifications');
+  };
+  xhr.onerror = function() {
+    alert('unable to register for notifications');
+  };
+  xhr.send();
+}
+
+function registerForPush(){
+  console.log('trying to register with gcm');
+  var senderIds = ["448301113255"];
+  chrome.gcm.register(senderIds, registerCallback);
+
+}
+
+// chrome.storage.local.set({registered: false});
+chrome.runtime.onStartup.addListener(function() {
+// chrome.tabs.onHighlighted.addListener(function(){
+  chrome.storage.local.get("registered", function(result) {
+    // If already registered, bail out.
+    if (result["registered"])
+      return;
+
+    // Up to 100 senders are allowed.
+    registerForPush();
+    // chrome.gcm.register(senderIds, registerCallback);
+  });
+});
+
+
+chrome.gcm.onMessage.addListener(function(message) {
+  console.log('received notification here asfasflkj');
+  console.log(message['data']);
+  createPushNotification(message['data']);
+});
 
---
+++ contentscript.js
@@ -0,0 +1,18 @@
+// // // $('div').prepend('<img src = "#" height = 10></img>');
+// // alert('replacing stuff')
+// // document.body.innerHTML = document.body.innerHTML.replace(/gasdlfkjsldfkj::/g, '<a href = "#">click me</a>');
+// var pattern=/pbl.link/;
+// // var viewtext_base_url = "http://viewtext.org/article?url=";
+// // var newurl;
+// if (pattern.test(window.document.URL)) // if it matches pattern defined above
+// {
+//   console.log('pattern matched cs');
+//   newurl = 'http://testing.berkeley-pbl.com/'+window.document.URL.split('pbl.link\/')[1]+'/go'
+//   chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
+
+// }
+// else{
+// 	console.log(window.document.URL);
+// }
+
+// // chrome.extension.sendRequest({redirect: window.document.URL + "lskdfjl"});
